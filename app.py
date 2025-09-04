3fdeafbee7493a41d14b8d1b26538ab0b1934092
jsarachin <joaosarachin@hotmail.com>
2025-09-04 20:59:12 -0300

    Incluindo vulnerabilidades.

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def list_users():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # cria a tabela automaticamente
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000) # nosec B104
